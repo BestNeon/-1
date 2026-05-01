fb7a408e63eda103638b839d2bbc6af945a686ef
S1pLanck <3022492236@qq.com>
2026-05-01 17:02:15 +0800

    测试

---
+++ cpp1.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int num = 0;
+    int a, b; // 补上这一行，声明变量 a 和 b
+    cin >> a >> b;
+    num = a + b;
+    cout << num << endl;
+    return 0;
+}
---
+++ cpp1.h
@@ -0,0 +1,13 @@
+//
+// Created by 30224 on 2026/5/1.
+//
+
+#ifndef INC_1_CPP1_H
+#define INC_1_CPP1_H
+1
+class cpp1
+{
+};
+
+
+#endif //INC_1_CPP1_H
